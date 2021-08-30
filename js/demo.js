5f491889f1440b4f56d8b850f544c1559116f4f8
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-08-30 14:31:50 -0700

    Roll feature callouts into the demo itself

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,9 @@
 $(function () {
-  var term = new window.Terminal();
-  term.open(document.getElementById('terminal'));
+  var term = new window.Terminal({
+    fontFamily: '"Cascadia Code", Menlo, monospace',
+    // rendererType: 'dom'
+  });
+  term.open(document.querySelector('.demo .inner'));
 
   function runFakeTerminal() {
     if (term._initialized) {
@@ -12,6 +15,29 @@ $(function () {
     term.prompt = () => {
       term.write('\r\n$ ');
     };
+
+    // TODO: Use a nicer default font
+    term.writeln([
+      '    Xterm.js is the frontend component that powers many terminals including',
+      '                           \x1b[3mVS Code\x1b[0m, \x1b[3mHyper\x1b[0m and \x1b[3mTheia\x1b[0m!',
+      '',
+      ' ┌ \x1b[1mFeatures\x1b[0m ──────────────────────────────────────────────────────────────────┐',
+      ' │                                                                            │',
+      ' │  \x1b[1mApps just work                         Performance\x1b[0m                        │',
+      // TODO: Link to https://www.npmjs.com/package/xterm-addon-webgl
+      ' │    Xterm.js works with most terminal     Xterm.js is \x1b[3mfast\x1b[0m and includes an  │',
+      ' │    apps like bash, vim and tmux          optional WebGL-based renderer     │',
+      ' │                                                                            │',
+      ' │  \x1b[1mUnicode support                        Self-contained\x1b[0m                     │',
+      ' │    Supports CJK, emojis and IMEs         Zero external dependencies        │',
+      ' │                                                                            │',
+      ' │  \x1b[1mAccessible                             And much more...\x1b[0m                   │',
+      // TODO: Links/buttons
+      ' │    A screen reader mode is available     Links, themes, addons, typed API  │',
+      ' │                                                                            │',
+      ' └────────────────────────────────────────────────────────────────────────────┘',
+      ''
+    ].join('\n\r'));
 
     term.writeln('Welcome to xterm.js');
     term.writeln('This is a local terminal emulation, without a real terminal in the back-end.');
@@ -40,6 +66,6 @@ $(function () {
 
   function prompt(term) {
     term.write('\r\n$ ');
-  }
+  }  
   runFakeTerminal();
 });
